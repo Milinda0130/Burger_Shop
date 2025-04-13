2ddaa288342149a3898e5abc8c2db4ae87a13fa3
Milinda0130 <milindanawarathne20020130@gmail.com>
2025-04-13 22:21:20 +0530

    animation part done

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function loadItems(filteredItems = itemlist) {
                 <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     <a href="#">${item.name}</a>
                 </h3>
-                <p>Rs.${item.price}</p>
+               <p class="price">Rs.${item.price}</p>
             </div>
             <button class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 onclick="addToCart(${index})">
